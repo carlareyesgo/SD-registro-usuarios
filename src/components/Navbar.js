1b149b0834c62f24a930ba536c6c39c8392e5402
Carla Godínez <carlareyesgo@gmail.com>
2022-12-22 23:35:41 -0700

    Logo del sitio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,16 @@ import { useState } from 'react';
 import { Navbar, Container, Offcanvas, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { publicRoutes } from '../router';
+import imgLogo from '../assets/img/logo.svg';
 
 const NavbarComponent = () => {
     const [menuOpen, setMenuOpen] = useState(false);
     const toggleMenu = () => setMenuOpen(!menuOpen);
 
-
     return (
         <Navbar bg="light" className="mb-3" expand='lg' collapseOnSelect>
             <Container fluid>
-                <Navbar.Brand href="#">Salud Digna</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/"><img src={imgLogo} alt='logo' /></Navbar.Brand>
                 <Navbar.Toggle aria-controls={`main-nav`} onClick={toggleMenu} />
                 <Navbar.Offcanvas
                     id={`main-nav`}
@@ -21,8 +21,8 @@ const NavbarComponent = () => {
                     onHide={toggleMenu}
                 >
                     <Offcanvas.Header closeButton>
-                        <Offcanvas.Title id={`main-nav-label`}>
-                            Salud Digna
+                        <Offcanvas.Title id={`main-nav-label`} as={Link} to="/" onClick={() => setMenuOpen(false)}>
+                            <img src={imgLogo} alt='logo' />
                         </Offcanvas.Title>
                     </Offcanvas.Header>
                     <Offcanvas.Body>
