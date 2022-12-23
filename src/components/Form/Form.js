a02782d0cffa68e2344ac96a52394c7251426d4d
Carla Godínez <carlareyesgo@gmail.com>
2022-12-22 20:58:45 -0700

    Form - Submit Handler

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,200 +1,9 @@
-import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
-import { rulesHandler } from "../../utils";
 import Input from "./Input/Input";
 
-const FormComponent = ({ inputs }) => {
-    const [formInputs, setFormInputs] = useState([
-        {
-            id: 1,
-            type: 'text',
-            label: 'Nombre(s)',
-            isTouched: false,
-            validationRules: [
-                {
-                    type: 'required',
-                    ruleFactor: true,
-                    errorMessage: 'El nombre es un dato requerido.',
-                    isValid: false,
-                },
-                {
-                    type: 'min',
-                    ruleFactor: 3,
-                    errorMessage: 'El nombre debe contener al menos 3 caracteres.',
-                    isValid: false,
-                },
-                {
-                    type: 'regex',
-                    ruleFactor: /^[A-Za-zÁÉÍÓÚáéíóúñÑ ]+$/g,
-                    errorMessage: 'El nombre sólo puede contener letras.',
-                    isValid: false,
-                }
-            ],
-            isValid: false,
-            value: '',
-        },
-        {
-            id: 2,
-            type: 'text',
-            label: 'Apellido Paterno',
-            isTouched: false,
-            validationRules: [
-                {
-                    type: 'required',
-                    ruleFactor: true,
-                    errorMessage: 'El apellido paterno es un dato requerido.',
-                    isValid: false,
-                },
-                {
-                    type: 'min',
-                    ruleFactor: 2,
-                    errorMessage: 'El apellido paterno debe contener al menos 2 caracteres.',
-                    isValid: false,
-                },
-                {
-                    type: 'regex',
-                    ruleFactor: /^[A-Za-zÁÉÍÓÚáéíóúñÑ ]+$/g,
-                    errorMessage: 'El apellido paterno sólo puede contener letras.',
-                    isValid: false,
-                }
-            ],
-            isValid: false,
-            value: '',
-        },
-        {
-            id: 3,
-            type: 'text',
-            label: 'Apellido Materno',
-            isTouched: false,
-            validationRules: [
-                {
-                    type: 'required',
-                    ruleFactor: true,
-                    errorMessage: 'El apellido materno es un dato requerido.',
-                    isValid: false,
-                },
-                {
-                    type: 'min',
-                    ruleFactor: 2,
-                    errorMessage: 'El apellido materno debe contener al menos 2 caracteres.',
-                    isValid: false,
-                },
-                {
-                    type: 'regex',
-                    ruleFactor: /^[A-Za-zÁÉÍÓÚáéíóúñÑ ]+$/g,
-                    errorMessage: 'El apellido materno sólo puede contener letras.',
-                    isValid: false,
-                }
-            ],
-            isValid: false,
-            value: '',
-        },
-        {
-            id: 5,
-            type: 'tel',
-            label: 'Teléfono',
-            isTouched: false,
-            validationRules: [
-                {
-                    type: 'required',
-                    ruleFactor: true,
-                    errorMessage: 'El teléfono es un dato requerido.',
-                    isValid: false,
-                },
-                {
-                    type: 'equal',
-                    ruleFactor: 10,
-                    errorMessage: 'El teléfono debe contener 10 números.',
-                    isValid: false,
-                },
-                {
-                    type: 'regex',
-                    ruleFactor: /^[0-9]+$/g,
-                    errorMessage: 'El teléfono sólo puede contener números.',
-                    isValid: false,
-                }
-            ],
-            isValid: false,
-            value: '',
-        },
-        {
-            id: 6,
-            type: 'email',
-            label: 'Correo',
-            isTouched: false,
-            validationRules: [
-                {
-                    type: 'required',
-                    ruleFactor: true,
-                    errorMessage: 'El correo es un dato requerido.',
-                    isValid: false,
-                },
-                {
-                    type: 'regex',
-                    ruleFactor: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-                    errorMessage: 'El correo sólo puede contener letras.',
-                    isValid: false,
-                }
-            ],
-            isValid: false,
-            value: '',
-        },
-        {
-            id: 4,
-            type: 'radio',
-            options: [
-                {
-                    key: 'F',
-                    label: 'Femenino'
-                },
-                {
-                    key: 'M',
-                    label: 'Masculino'
-                }
-            ],
-            label: 'Genero',
-            isTouched: false,
-            validationRules: [
-                {
-                    type: 'required',
-                    ruleFactor: true,
-                    errorMessage: 'El genero es un dato requerido.',
-                    isValid: false,
-                },
-            ],
-            isValid: false,
-            value: '',
-        },
-    ]);
-
-    const touchedHandler = (id) => {
-        const oldFormInputs = [...formInputs];
-        const indexInput = oldFormInputs.findIndex((input) => input.id === id);
-        oldFormInputs[indexInput].isTouched = true;
-        setFormInputs(oldFormInputs);
-    };
-
-    const validationHandler = (input) => {
-        const { value, validationRules } = input;
-        let valueIsValid = true;
-        input.validationRules = validationRules.map((rule) => {
-            let ruleIsValid = rulesHandler[rule.type](value, rule.ruleFactor);
-            valueIsValid = valueIsValid && ruleIsValid;
-            return ({ ...rule, isValid: ruleIsValid })
-        });
-        input.isValid = valueIsValid;
-    };
-
-    const onChangeHandler = (event, id) => {
-        const oldFormInputs = [...formInputs];
-        const indexInput = oldFormInputs.findIndex((input) => input.id === id);
-        oldFormInputs[indexInput].value = event.target.value;
-        validationHandler(oldFormInputs[indexInput])
-        setFormInputs(oldFormInputs);
-    };
-
+const FormComponent = ({ formInputs, onSubmitHandler, touchedHandler, onChangeHandler, isButtonInactive }) => {
     return (
-        <Form>
+        <Form onSubmit={onSubmitHandler}>
             {formInputs.map((input) => (
                 <Input
                     key={input.id}
@@ -205,7 +14,12 @@ const FormComponent = ({ inputs }) => {
             ))}
             <br />
             <br />
-            <Button type="submit">Registrar</Button>
+            <Button
+                type="submit"
+                disabled={isButtonInactive}
+            >
+                Registrar
+            </Button>
         </Form>
     )
 
--- src/pages/RegistroPage.js
+++ src/pages/RegistroPage.js
@@ -1,34 +1,227 @@
+import { useState } from "react";
 import FormComponent from "../components/Form/Form";
+import { rulesHandler } from "../utils";
 
 const RegistroPage = () => {
-    const formInputs = [
+    const formInputsInit = [
         {
-            id: 0,
+            id: 1,
+            key: 'nombre',
+            type: 'text',
             label: 'Nombre(s)',
-            isValid: false,
             isTouched: false,
-            validation:[
+            validationRules: [
                 {
                     type: 'required',
-                    value: true,
+                    ruleFactor: true,
                     errorMessage: 'El nombre es un dato requerido.',
+                    isValid: false,
                 },
                 {
                     type: 'min',
-                    value: 3,
+                    ruleFactor: 3,
                     errorMessage: 'El nombre debe contener al menos 3 caracteres.',
+                    isValid: false,
                 },
                 {
                     type: 'regex',
-                    value: /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/,
+                    ruleFactor: /^[A-Za-zÁÉÍÓÚáéíóúñÑ ]+$/g,
                     errorMessage: 'El nombre sólo puede contener letras.',
+                    isValid: false,
                 }
             ],
+            isValid: false,
+            value: '',
+        },
+        {
+            id: 2,
+            key: 'aPaterno',
+            type: 'text',
+            label: 'Apellido Paterno',
+            isTouched: false,
+            validationRules: [
+                {
+                    type: 'required',
+                    ruleFactor: true,
+                    errorMessage: 'El apellido paterno es un dato requerido.',
+                    isValid: false,
+                },
+                {
+                    type: 'min',
+                    ruleFactor: 2,
+                    errorMessage: 'El apellido paterno debe contener al menos 2 caracteres.',
+                    isValid: false,
+                },
+                {
+                    type: 'regex',
+                    ruleFactor: /^[A-Za-zÁÉÍÓÚáéíóúñÑ ]+$/g,
+                    errorMessage: 'El apellido paterno sólo puede contener letras.',
+                    isValid: false,
+                }
+            ],
+            isValid: false,
+            value: '',
+        },
+        {
+            id: 3,
+            type: 'text',
+            key: 'aMaterno',
+            label: 'Apellido Materno',
+            isTouched: false,
+            validationRules: [
+                {
+                    type: 'required',
+                    ruleFactor: true,
+                    errorMessage: 'El apellido materno es un dato requerido.',
+                    isValid: false,
+                },
+                {
+                    type: 'min',
+                    ruleFactor: 2,
+                    errorMessage: 'El apellido materno debe contener al menos 2 caracteres.',
+                    isValid: false,
+                },
+                {
+                    type: 'regex',
+                    ruleFactor: /^[A-Za-zÁÉÍÓÚáéíóúñÑ ]+$/g,
+                    errorMessage: 'El apellido materno sólo puede contener letras.',
+                    isValid: false,
+                }
+            ],
+            isValid: false,
+            value: '',
+        },
+        {
+            id: 5,
+            key: 'telefono',
+            type: 'tel',
+            label: 'Teléfono',
+            isTouched: false,
+            validationRules: [
+                {
+                    type: 'required',
+                    ruleFactor: true,
+                    errorMessage: 'El teléfono es un dato requerido.',
+                    isValid: false,
+                },
+                {
+                    type: 'equal',
+                    ruleFactor: 10,
+                    errorMessage: 'El teléfono debe contener 10 números.',
+                    isValid: false,
+                },
+                {
+                    type: 'regex',
+                    ruleFactor: /^[0-9]+$/g,
+                    errorMessage: 'El teléfono sólo puede contener números.',
+                    isValid: false,
+                }
+            ],
+            isValid: false,
+            value: '',
+        },
+        {
+            id: 6,
+            key: 'correo',
+            type: 'email',
+            label: 'Correo',
+            isTouched: false,
+            validationRules: [
+                {
+                    type: 'required',
+                    ruleFactor: true,
+                    errorMessage: 'El correo es un dato requerido.',
+                    isValid: false,
+                },
+                {
+                    type: 'regex',
+                    ruleFactor: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+                    errorMessage: 'El correo sólo puede contener letras.',
+                    isValid: false,
+                }
+            ],
+            isValid: false,
+            value: '',
+        },
+        {
+            id: 4,
+            key: 'genero',
+            type: 'radio',
+            options: [
+                {
+                    key: 'F',
+                    label: 'Femenino'
+                },
+                {
+                    key: 'M',
+                    label: 'Masculino'
+                }
+            ],
+            label: 'Genero',
+            isTouched: false,
+            validationRules: [
+                {
+                    type: 'required',
+                    ruleFactor: true,
+                    errorMessage: 'El genero es un dato requerido.',
+                    isValid: false,
+                },
+            ],
+            isValid: false,
             value: '',
         },
     ];
+    const [formInputs, setFormInputs] = useState(formInputsInit);
+    const [isButtonInactive, setIsButtonInactive] = useState(true);
+
+    const touchedHandler = (id) => {
+        const oldFormInputs = [...formInputs];
+        const indexInput = oldFormInputs.findIndex((input) => input.id === id);
+        oldFormInputs[indexInput].isTouched = true;
+        setFormInputs(oldFormInputs);
+    };
+
+    const validationHandler = (input) => {
+        const { value, validationRules } = input;
+        let valueIsValid = true;
+        input.validationRules = validationRules.map((rule) => {
+            let ruleIsValid = rulesHandler[rule.type](value, rule.ruleFactor);
+            valueIsValid = valueIsValid && ruleIsValid;
+            return ({ ...rule, isValid: ruleIsValid })
+        });
+        input.isValid = valueIsValid;
+    };
+
+    const onChangeHandler = (event, id) => {
+        const oldFormInputs = [...formInputs];
+        const indexInput = oldFormInputs.findIndex((input) => input.id === id);
+        oldFormInputs[indexInput].value = event.target.value;
+        validationHandler(oldFormInputs[indexInput])
+        setFormInputs(oldFormInputs);
+        setIsButtonInactive(!formInputs.reduce((acc, current) => acc && current.isValid, true))
+    };
+
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        const formEntries = {};
+        formInputs.forEach((input) => {
+            formEntries[input.key] = input.value;
+        })
+        console.log(formEntries);
+        // TODO: SAVE
+        setFormInputs(formInputsInit);
+        setIsButtonInactive(true);
+    }
+
     return (
-        <FormComponent inputs={formInputs || []} />
+        <FormComponent
+            formInputs={formInputs || []}
+            onSubmitHandler={onSubmitHandler}
+            touchedHandler={touchedHandler}
+            onChangeHandler={onChangeHandler}
+            isButtonInactive={isButtonInactive}
+        />
     );
 }
 
